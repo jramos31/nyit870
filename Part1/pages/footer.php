b7455db701d4f5a2778812d12e1d2ad82bc45be5
dp-sp-2016 <dporras@nyit.edu>
2016-02-10 21:38:20 -0500

    Separated header, footer, and content code of index.html

---
+++ Part1/pages/footer.php
@@ -0,0 +1,32 @@
+    
+	</div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+	
+    <footer>
+        <p>
+            <a href="https://www.facebook.com/mynyit/">
+                <img src='http://icons.iconarchive.com/icons/sicons/basic-round-social/512/facebook-icon.png' alt="facebook">
+            </a>
+            <a href="https://www.instagram.com/nyit_photos/">
+                <img src='https://www.seeklogo.net/wp-content/uploads/2015/08/instagram-icon-circle-vector-logo.png' alt="instagram">
+            </a>
+        </p>
+        <p>Copyright &copy; 2016 D&J NYIT BlackBoard. All rights reserved.</p>
+        <p>Created by Dickson Porras & Jean Ramos.</p>
+    </footer>
+	
+</body>
+
+</html>
---
+++ Part1/pages/front-page.php
@@ -0,0 +1,110 @@
+
+<?php include("header.php"); ?>
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Course & Exam Training System</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                              <h4 class="panel-title">On Demand Help and Learning Catalog</h4>
+                            </div>
+                            <div class="panel-body fixed-panel-height">
+                                <p>Learn more about Blackboard Learn through our
+                                  <a href="#">On Demand Learning Center.</a>
+                                  The On Demand Learning Center includes short, interactive video
+                                  lessions called Quick Tutorials and short documents, called
+                                  Getting Started Guides, designed to get you familiar with a feature
+                                  in 15 minutes.</p>
+                            </div>
+                            <a href="#">
+                              <div class="panel-footer">
+                                  <span class="pull-left">View Details</span>
+                                  <span class="pull-right">
+                                      <i class="fa fa-arrow-circle-right"></i>
+                                  </span>
+                                  <div class="clearfix"></div>
+                              </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="panel panel-primary">
+                          <div class="panel-heading">
+                            <h4 class="panel-title">My Courses</h4>
+                          </div>
+                          <div class="panel-body fixed-panel-height">
+                              <strong>Courses where you are: Student</strong>
+                              <span style="font-size: 12px">
+                              <ul class="list-group">
+                                <li class="list-group-item">2013 Fall - M01 - Algorithm Concepts</li>
+                                <li class="list-group-item">2014 Spring - M01 - Theoretical Concepts in Computer and Computation</li>
+                                <li class="list-group-item">Fall 2015 - M03 - Software Engineering</li>
+                              </ul>
+                              <!-- <ul>
+                                <li>2013 Fall - M01 - Algorithm Concepts</li>
+                                <li>2014 Spring - M01 - Theoretical Concepts in Computer and Computation</li>
+                                <li>Fall 2015 - M03 - Software Engineering</li>
+                              </ul> -->
+                            </span>
+                          </div>
+
+
+                          <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right">
+                                    <i class="fa fa-arrow-circle-right"></i>
+                                </span>
+                                <div class="clearfix"></div>
+                            </div>
+                          </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="panel panel-primary">
+                          <div class="panel-heading">
+                            <h4 class="panel-title">My Courses</h4>
+                          </div>
+
+                          <ul class="list-group">
+                            <li class="list-group-item">No Institution Announcements have been posted in the last 7 days.</li>
+                            <li class="list-group-item">No Course or Organizational Announcements have been posted in the last 7 days.</li>
+                          </ul>
+                          <div class="panel-footer">
+                              <a href="#"><span class="pull-right">More announcements...</span></a>
+                              <div class="clearfix"></div>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="panel panel-primary">
+                          <div class="panel-heading">
+                            <h4 class="panel-title">My Tasks</h4>
+                          </div>
+                          <div class="panel-body">
+                            <p>My Tasks:</p>
+                          </div>
+                          <ul class="list-group">
+                            <li class="list-group-item">No Tasks Due.</li>
+                          </ul>
+                          <div class="panel-footer">
+                              <a href="#"><span class="pull-right">More tasks...</span></a>
+                              <div class="clearfix"></div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include("footer.php"); ?>
