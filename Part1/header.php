e93604ba348c849d3f428ea2e93228fc9bfaf850
jramos31 <jramos04@nyit.edu>
2016-03-19 12:06:52 -0400

    Changed index.php and fix spacing

--- Part1/header.php
+++ Part1/header.php
@@ -1,129 +1,88 @@
 <?php
-
-// Begin output buffering and initialize a session 
+/*
+* the default header file
+*/
 ob_start();
 session_start();
-?>
+?> <!-- Begin output buffering and initialize a session  -->
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="../../favicon.ico">
+        <title>D-J Blackboard</title>
 
-    <title>DP-JR Blackboard</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <!-- Custom styles for this template -->
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">D&J Blackboard</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Homeboard</a></li>
+                        <li><a href="#">Settings</a></li>
+                        <li><a href="view_my_info.php">Profile</a></li>
+                        <li><a href="#">Help</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-    <!-- Custom styles for this template -->
-    <link href="style.css" rel="stylesheet">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3 col-md-2 sidebar">
+                    <h3>Tools</h3>
+                        <ul class="nav nav-sidebar">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#">Announcements</a></li>
+                            <li><a href="#">To-Do List</a></li>
+                            <li><a href="#">Address Book</a></li>
+                            <li><a href="#">Calendar</a></li>
+                            <li><a href="view_my_info.php">My Info</a></li>
+                            <?php
+                            /********  Display links depending on log in status  *********/
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+                            if (isset($_SESSION['user_id'])) {  // User is logged in
 
-  <body>
+                            echo '<li><a href="logout.php">Logout</a></li>
+                            <li><a href="change_password.php">Change Password</a></li>';
+                            }
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">D&J Blackboard</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Homeboard</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="view_my_info.php">Profile</a></li>
-            <li><a href="#">Help</a></li>
-          </ul>		  
-        </div>
-      </div>
-    </nav>
+                            if ($_SESSION['user_level'] == '0') { // User is Student
+                            echo '<li class="active"><a href="#">Student Options <span class="sr-only">(current)</span></a></li>
+                            <li><a href="view_grades.php">My Grades</a></li>
+                            <li><a href="view_courses.php">My Courses</a></li>';
+                            }
 
-	
-    <div class="container-fluid">
-      <div class="row">
-	  
-		<!-- ******  Start of Nav Sidebar  ****** -->
-        <div class="col-sm-3 col-md-2 sidebar">
-		
-		<!--	
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
-          </ul>
-		-->
-		
-		  <ul class="nav nav-sidebar">
-            <li><a href="index.php">Home</a></li>
-			<li><a href="#">Announcements</a></li>
-			<li><a href="#">To-Do List</a></li>
-			<li><a href="#">Address Book</a></li>
-			<li><a href="#">Calendar</a></li>
-			<li><a href="view_my_info.php">My Info</a></li>
-			<?php 
-			/********  Display links depending on log in status  *********/
-			
-			if (isset($_SESSION['user_id'])) {  // User is logged in
-				
-				echo '<li><a href="logout.php">Logout</a></li>
-					  <li><a href="change_password.php">Change Password</a></li>';
-				
-				if ($_SESSION['user_level'] == '0') { // User is Student
-					echo '<li class="active"><a href="#">Student Options <span class="sr-only">(current)</span></a></li>
-						  <li><a href="view_grades.php">My Grades</a></li>
-					      <li><a href="view_courses.php">My Courses</a></li>';
-				}
-				
-				if ($_SESSION['user_level'] == '1') { // User is Professor/Faculty
-					echo '<li><a href="view_courses.php">My Courses</a></li>
-					      <li><a href="">Faculty Link 2</a></li>';
-				}
-				
-				if ($_SESSION['user_level'] == '2') { // User is Administrator
-					echo '<li><a href="view_users.php">View All Users</a></li>
-					      <li><a href="">Other Admin Stuff</a></li>';
-				}
-			} else {  // User Not Logged In
-				echo '<li><a href="register.php">Register</a></li>
-					  <li><a href="login.php">Login</a></li>
-					  <li><a href="">Retrieve Password</a></li>';
-			}
-			?>
-			
-          </ul>
-		  
-		  <!-- 
-		  <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
-          </ul>
-		  
-		  <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-          </ul>
-		  -->
-		  
+                            if ($_SESSION['user_level'] == '1') { // User is Professor/Faculty
+                            echo '<li><a href="view_courses.php">My Courses</a></li>
+                            <li><a href="">Faculty Link 2</a></li>';
+                            }
+
+                            if ($_SESSION['user_level'] == '2') { // User is Administrator
+                            echo '<li><a href="view_users.php">View All Users</a></li>
+                            <li><a href="">Other Admin Stuff</a></li>';
+                            } else {  // User Not Logged In
+                            echo '<li><a href="register.php">Register</a></li>
+                            <li><a href="login.php">Login</a></li>
+                            <li><a href="">Retrieve Password</a></li>';
+                            }
+                            ?>
+                        </ul>
         </div>		
-		<!-- ******  End of Nav Sidebar  ****** -->
-		
--- Part1/index.php
+++ Part1/index.php
@@ -1,47 +1,40 @@
 <?php
 // *** Main page for the site  ***
 
-// Include the configuration and header files 
+// Include the configuration and header files
 require('config.inc.php');
 include('header.php');
 
 ?>
-
-		<!--  ****** Start of Page Content ******  -->
+        <!--  ****** HomeBoard ******  -->
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Homeboard</h1>
-		  <?php 
-			// Welcome the user (welcome them by name if they're logged in)
-			echo '<p><h4>Welcome';
-			if (isset($_SESSION['first_name'])) {
-				echo ", {$_SESSION['first_name']}";
-			}
-			echo '!</h4></p>';
-		  ?>
-          <div class="row placeholders">
-            <div class="col-xs-6 col-sm-3 placeholder">
-			
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Announcements</h4>
-              <span class="text-muted">Something else</span>
+            <h1 class="page-header">Homeboard</h1>
+                <?php
+                // Welcome the user (welcome them by name if they're logged in)
+                echo '<p><h4>Welcome';
+                if (isset($_SESSION['first_name'])) {
+                echo ", {$_SESSION['first_name']}";
+                }
+                echo '!</h4></p>';
+                ?>
+            <div class="row placeholders">
+                <div class="col-xs-6 col-sm-3 placeholder">
+                    <img src="https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/512/advertising.png" class="img-responsive" alt="Anouncement Image">
+                    <h4>Announcements</h4>
+                </div>
+                <div class="col-xs-6 col-sm-3 placeholder">
+                    <img src="http://104.236.10.194/wp-content/uploads/2015/08/Test-1.jpg1.png" class="img-responsive" alt="Courses Image">
+                    <h4><a href="view_courses.php">Courses</a></h4>
+                </div>
+                <div class="col-xs-6 col-sm-3 placeholder">
+                    <img src="http://www.devopstesting.com/wp-content/uploads/2014/08/Software-Testing-Services.png" class="img-responsive" alt="Online Exam Image">
+                    <h4>Online Exams</h4>
+                </div>
+                <div class="col-xs-6 col-sm-3 placeholder">
+                    <img src="http://manch.co.in/site_media/img/homepage/test-prep-icon.png" class="img-responsive" alt="Grades Image">
+                    <h4><a href="view_grades.php">Grades</a></h4>
+                </div>
             </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4><a href="view_courses.php">Courses</a></h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Online Exams</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4><a href="view_grades.php">Grades</a></h4>
-              <span class="text-muted">Something else</span>
-            </div>
-          </div>          
         </div>
-		<!--  ****** End of Page Content ******  -->
-		
+        
 <?php include('footer.php'); ?>
