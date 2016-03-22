9c0eed9a2a33a5b4170969317a20839768b4908b
jramos31 <jramos04@nyit.edu>
2016-03-21 22:47:32 -0400

    Sidebar/Footer  Adjustments

--- Part1/header.php
+++ Part1/header.php
@@ -16,82 +16,46 @@ session_start();
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <!-- Custom styles for this template -->
         <link href="style.css" rel="stylesheet">
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        
     </head>
     <body>
-        <nav class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">D&J Blackboard</a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Homeboard</a></li>
-                        <li><a href="#">Settings</a></li>
-                        <li><a href="view_my_info.php">Profile</a></li>
-                        <li><a href="#">Help</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
         <div class="container-fluid">
             <div class="row">
-                <!--<div class="col-sm-3 col-md-2 sidebar">
-                    <ul class="nav nav-sidebar">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="#">Announcements</a></li>
-                        <li><a href="#">To-Do List</a></li>
-                        <li><a href="#">Address Book</a></li>
-                        <li><a href="#">Calendar</a></li>
-                        <li><a href="view_my_info.php">My Info</a></li> -->
-                        <aside class="sidebar-left">
-                		<a class="company-logo" href="#">Logo</a>
-                    		<div class="sidebar-links">
-                    			<a class="link-blue" href="#"><i class="fa fa-picture-o"></i>Photography</a>
-                    			<a class="link-red" href="#"><i class="fa fa-heart-o"></i>Favorites</a>
-                    			<a class="link-yellow selected" href="#"><i class="fa fa-keyboard-o"></i>Projects</a>
-                    			<a class="link-green" href="#"><i class="fa fa-map-marker"></i>Places</a>
-                    		</div>
-                	   </aside>
+                <aside class="sidebar-left">
+                    <a class="company-logo" href="index.php">D-J</a>
+                    <div class="sidebar-links">
+                        <a href="index.php">Home</a>
+                        <a href="view_my_info.php"></i>My Info</a>
+                        <a href="#">Calendar</a>
+                        <a href="#">Announcements</a>
                         <?php
                         if (isset($_SESSION['user_id'])) {  // User is logged in
 
-                        echo '<li><a href="logout.php">Logout</a></li>
-                              <li><a href="change_password.php">Change Password</a></li>';
+                        echo '<a href="logout.php">Logout</a>
+                              <a href="change_password.php">Change Password</a>';
 
                         if ($_SESSION['user_level'] == '0') { // User is Student
-                        echo '<li class="active"><a href="#">Student Options <span class="sr-only">(current)</span></a></li>
-                              <li><a href="view_grades.php">My Grades</a></li>
-                              <li><a href="view_courses.php">My Courses</a></li>';
+                        echo '<a href="#">Student Options <span class="sr-only">(current)</span></a>
+                              <a href="view_grades.php">My Grades</a>
+                              <a href="view_courses.php">My Courses</a>';
                         }
 
                         if ($_SESSION['user_level'] == '1') { // User is Professor/Faculty
-                        echo '<li><a href="view_courses.php">My Courses</a></li>
-                              <li><a href="">Faculty Link 2</a></li>';
+                        echo '<a href="view_courses.php">My Courses</a>
+                              <a href="">Faculty Link 2</a>';
                         }
 
                         if ($_SESSION['user_level'] == '2') { // User is Administrator
-                        echo '<li><a href="view_users.php">View All Users</a></li>
-                              <li><a href="">Other Admin Stuff</a></li>';
+                        echo '<a href="view_users.php">View All Users</a>
+                              <a href="">Other Admin Stuff</a>';
                         }
                         } else {  // User Not Logged In
-                        echo '<li><a href="register.php">Register</a></li>
-                              <li><a href="login.php">Login</a></li>
-                              <li><a href="">Retrieve Password</a></li>';
+                        echo '<a href="register.php">Register</a>
+                              <a href="login.php">Login</a>
+                              <a href="">Retrieve Password</a>';
                         }
                         ?>
-                    <!--</ul> -->
-                </div>
+                    </div>
+                </aside>
             </div>
         </div>
